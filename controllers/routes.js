b886ce3fa8bc1f3e495059e2ef993149fc2cb8d0
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-02-11 14:11:39 -0800

    added monthly data

--- controllers/routes.js
+++ controllers/routes.js
@@ -84,4 +84,64 @@ module.exports = app => {
       }
     })
   })
+
+  app.get('/:user/monthly', (req, res) => {
+
+    let data = [];
+
+    request('https://github.com/' + req.params.user, function (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        //Grabs information from given page, goes to item with id #Current_officeholders
+        $('div.js-calendar-graph > svg > g > g > rect').each(function(i, element){
+            //console.log(raw)
+            const contributions = $(this).attr('data-count');
+            // console.log(contributions)
+            const dataDate = $(this).attr('data-date');
+            // console.log(dataDate)
+            let object = {
+              contribution: contributions,
+              dataDate: dataDate
+            };
+            data.push(object)
+            // console.log('added obj')
+        })
+        console.log('returning data')
+        let months = {
+          0: [],
+          1: [],
+          2: [],
+          3: [],
+          4: [],
+          5: [],
+          6: [],
+          7: [],
+          8: [],
+          9: [],
+          10: [],
+          11: []
+        }
+        for (item in data){
+          day = new Date(data[item].dataDate)
+          months[day.getMonth()].push(data[item].contribution)
+        }
+        console.log(months)
+        monthNames = ['January' , 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        monthAvg = []
+        for (month in months){
+          let sum = 0
+          for (dayData in months[month]){
+            sum += parseInt(months[month][dayData])
+          }
+          monthAvg.push({
+            Day: monthNames[month],
+            totalContributions: sum,
+            averageContributions: sum/months[month].length
+          })
+        }
+        console.log(monthAvg)
+        res.json(monthAvg)
+      }
+    })
+  })
 }
