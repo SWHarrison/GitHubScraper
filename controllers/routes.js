2f283ed4a8e614422e1d7929193b9f8706e9c44d
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-02-11 13:39:34 -0800

    added daily averages route

--- controllers/routes.js
+++ controllers/routes.js
@@ -22,12 +22,65 @@ module.exports = app => {
               dataDate: dataDate
             };
             data.push(object)
-            console.log('added obj')
+            // console.log('added obj')
         })
         console.log('returning data')
         res.json(data)
       }
-      console.log(data)
+    })
+  })
+
+  app.get('/:user/daily', (req, res) => {
+
+    let data = [];
+
+    request('https://github.com/' + req.params.user, function (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        //Grabs information from given page, goes to item with id #Current_officeholders
+        $('div.js-calendar-graph > svg > g > g > rect').each(function(i, element){
+            //console.log(raw)
+            const contributions = $(this).attr('data-count');
+            // console.log(contributions)
+            const dataDate = $(this).attr('data-date');
+            // console.log(dataDate)
+            let object = {
+              contribution: contributions,
+              dataDate: dataDate
+            };
+            data.push(object)
+            // console.log('added obj')
+        })
+        console.log('returning data')
+        let days = {
+          0: [],
+          1: [],
+          2: [],
+          3: [],
+          4: [],
+          5: [],
+          6: []
+        }
+        for (item in data){
+          day = new Date(data[item].dataDate)
+          days[day.getDay()].push(data[item].contribution)
+        }
+        console.log(days)
+        dayNames = ['Sunday' , 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        daysAvg = []
+        for (day in days){
+          let sum = 0
+          for (dayData in days[day]){
+            sum += days[day][dayData]
+          }
+          daysAvg.push({
+            Day: dayNames[day],
+            averageContributions: sum/days[day].length
+          })
+        }
+        console.log(daysAvg)
+        res.json(daysAvg)
+      }
     })
   })
 }
