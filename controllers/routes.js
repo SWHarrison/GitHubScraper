dea0c2e95f966a26c687af3d0e4c72dc82b527fc
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-02-13 15:32:07 -0800

    updated /:user route

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,6 +7,23 @@ module.exports = app => {
 
   app.get('/:user', (req, res) => {
 
+    console.log("searching for " + req.params.user)
+    User.findOne({
+      username: req.params.user
+    }).then(user => {
+      if (!user) {
+        new_user = new User()
+        new_user.username = req.params.user
+        new_user.numberOfRequests = 1
+        console.log(new_user)
+        new_user.save()
+      } else {
+        console.log("found user")
+        console.log(user)
+        user.numberOfRequests += 1
+        user.save()
+      }
+    })
     let data = [];
 
     request('https://github.com/' + req.params.user, function (error, response, html) {
@@ -27,22 +44,6 @@ module.exports = app => {
             // console.log('added obj')
         })
         console.log('returning data')
-        User.findOne({
-          username: req.params.user
-        }).then(user => {
-          if (!user) {
-            new_user = new User()
-            new_user.username = req.params.user
-            new_user.numberOfRequests = 1
-            console.log(new_user)
-            new_user.save()
-          } else {
-            console.log("found user")
-            console.log(user)
-            user.numberOfRequests += 1
-            user.save()
-          }
-        })
         res.json(data)
       }
     })
